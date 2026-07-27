1f2ede4625cea51a703be44c180592e67d6ad65f
pritamsinghrathore78988 <pritamsinghrathore78988@gmail.com>
2026-07-27 13:12:59 +0530

    Update 0048-rotate-image.java

--- 0048-rotate-image/0048-rotate-image.java
+++ 0048-rotate-image/0048-rotate-image.java
@@ -1,8 +1,6 @@
 class Solution {
     public void rotate(int[][] matrix) {
         int n = matrix.length;
-
-        // Step 1: Transpose the matrix
         for (int i = 0; i < n; i++) {
             for (int j = i + 1; j < n; j++) {
                 int temp = matrix[i][j];
@@ -10,17 +8,13 @@ class Solution {
                 matrix[j][i] = temp;
             }
         }
-
-        // Step 2: Reverse each row
         for (int i = 0; i < n; i++) {
             int left = 0;
             int right = n - 1;
-
             while (left < right) {
                 int temp = matrix[i][left];
                 matrix[i][left] = matrix[i][right];
                 matrix[i][right] = temp;
-
                 left++;
                 right--;
             }
